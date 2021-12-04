9ef78d7b7448836f8736a95996a8c1ba92ea1330
leaisrevolution <leaisrevolution@gmail.com>
2021-12-05 03:42:04 +0900

    Navber 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
     return (
-    <div>
+        <>
+            <Navbar bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand href="#home">Lea's Shopping Mall</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            </Container>
+            </Navbar>
 
-    </div>
+            <div>
+            </div>
+
+
+        </>
     )
 }
 
