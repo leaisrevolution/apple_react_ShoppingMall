f6d93f9dbdf1498be9d796d30d4aa08f6ad6b914
leaisrevolution <leaisrevolution@gmail.com>
2021-12-04 23:41:01 +0900

    JSX를 이용하여 HTML 페이지 제작해보기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+// React는 class대신 className을 사용한다.
+// 서버에서 가져온 데이터를 바인딩(HTML에 넣음)이 매우 쉽다 -> 그래서 리액트를 씀
+// src, id, href 등 속성에도 {변수명, 함수} 사용 가능
+// style 사용시 {} 중괄호로 사용, CamelCase 사용 (변수에 넣어서도 사용 가능)
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let posts = '강남 고기 맛집';
+    let stylePosts = { color : 'blue', fontSize : '30px'}
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <div style={stylePosts}> 레아의 Blog</div>
+            </div>
+            <h4>{posts}</h4>
+        </div>
+    );
 }
 
 export default App;
